a98f7e65060c4a3cc42d63f4665d5d76c6ae23f4
nivetha-ashokkuma <nivetha7620@gmail.com>
2021-11-01 14:58:12 +0530

    tested git methods

---
+++ dict_function.py
@@ -0,0 +1,20 @@
+details = {"name" : "nivetha", "age" : 21, "dept" : "CSE"}
+print(details)
+
+details = {"name" : "nivetha", "age" : 21, "dept" : "CSE"}
+print(len(details))
+
+details = {"name" : "nivetha", "age" : 21, "dept" : "CSE"}
+print(details.keys())
+
+details = {"name" : "nivetha", "age" : 21, "dept" : "CSE"}
+print(details.values())
+
+details = {"name" : "nivetha", "age" : 21, "dept" : "CSE"}
+print(details.items())
+
+details1 = {"name" : "nivetha", "age" : 21, "dept" : "CSE"}
+details2 = {"age" : 25}
+details1.update(details2)
+
+print(details1)
