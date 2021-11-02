2d4bb0108e60c710a7e2ebd74de55668c413289e
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-02 10:15:25 +0530

    arguments and keywordarguments based commit

---
+++ arguments.py
@@ -0,0 +1,8 @@
+def sum(*args, **kwargs):
+    sum = args1 + kwargs1
+    return sum
+
+args1 = int(input("enter args value:"))
+kwargs1 = int(input("enter kwargs value:"))
+result = sum(args1, kwargs1)
+print(result)
