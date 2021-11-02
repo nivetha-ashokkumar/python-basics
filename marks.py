6137ae726a1002711650cc8c44eb7fa780b64b9f
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-02 10:08:46 +0530

    student marks commit

---
+++ marks.py
@@ -0,0 +1,15 @@
+def details(name, age, mark):
+    if(mark>80):
+        return "congrats"
+    elif(mark > 60) and (mark < 80):
+        return "try to do better"
+    elif(mark < 60) and (mark > 40):
+        return "practice more"
+    else:
+        return "fail"
+name = input("enter your name:")
+age = int(input("enter your age:"))
+mark = int(input("enter your mark:"))
+result = details(name, age, mark)
+print(result)
+    
