7f2cb244fc62323a1b559efce952cda205098ae6
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-02 10:02:29 +0530

    addition commit

---
+++ addition_operation.py
@@ -0,0 +1,10 @@
+def sum(num1, num2):
+    sum = num1 + num2
+    return sum
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+result = sum(num1, num2)
+print(result)
+
+
+    
