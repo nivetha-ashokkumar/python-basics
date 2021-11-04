2ef7478d3ecef5760fde337786daf104bfd9372c
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 21:08:40 +0530

    testcase commit

---
+++ calculate_testcase.py
@@ -0,0 +1,28 @@
+import unittest
+from calculate import calculate_addition, calculate_subtraction
+
+
+class Testcalculate_addition(unittest.TestCase):
+    def test_001_calculate_addition(self):
+        a = 10
+        b = 20
+        c = calculate_addition(a, b)
+        self.assertEqual(c, 30)
+    
+    def test_001_calculate_subtraction(self, a, b):
+        a = 20
+        b = 10
+        c = calculate_subtraction(a,b)
+        self.assertEqual(c, 10)
+    
+    def test_001_calculate_addition(self, a, b):
+        a = 5.0
+        b = 4.9
+        c = calculate_addition(a,b)
+        self.assertEqual(c, 9.9)
+
+    def test_001_calculate_addition(self, a, b):
+        a = "ab"
+        b = "cd"
+        c = calculate_addition(a,b)
+        self.assertEqual(c, "abcd")
