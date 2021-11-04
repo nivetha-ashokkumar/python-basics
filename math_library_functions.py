d738fedbe6239475cb4cd9ec1c00317fa7af870d
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 21:36:03 +0530

    library commit

---
+++ math_library_functions.py
@@ -0,0 +1,16 @@
+import math
+
+first_number = int(input('enter first value'))
+second_number = int(input('enter second value'))
+print(math.sqrt(first_number))
+print(math.pow(first_number, second_number))
+print(math.ceil(second_number))
+print(math.fabs(first_number))
+print(math.factorial(first_number))
+print(math.floor(first_number))
+print(math.fmod(first_number, second_number))
+print(math.gcd(first_number, second_number))
+
+
+
+
