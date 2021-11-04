f67b45332fe5edb802beb50de5bba2ea46d53eb4
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 21:25:46 +0530

    calculation commit

---
+++ calculate.py
@@ -0,0 +1,21 @@
+def calculate_addition(a ,b):
+    return a + b
+
+
+def calculate_subtraction(a, b):
+    return a - b
+
+a = int(input("enter a value:"))
+b = float(input("enter b value"))
+c = calculate_addition(a, b)
+print(c)
+    
+def check(c):
+    if(c == str):
+        print(str)
+    else:
+        print(c)
+
+d = calculate_subtraction(a,b)
+print(d)
+
