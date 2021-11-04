18e37f82eae0779971d7f19bf6565a7858cf2201
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 21:33:29 +0530

    introduction based commit

---
+++ intro.py
@@ -0,0 +1,35 @@
+
+class Intro:
+    name = None
+    native = None
+    education = None
+    family_members = 0
+
+nivetha = Intro()
+nivetha.name = "nivetha"
+nivetha.native = "kalavai"
+nivetha.education = "B.E"
+nivetha.family_members = 6
+
+print(nivetha.__dict__)
+
+pavithra = Intro()
+pavithra.name = "pavithra"
+pavithra.native = "kalavai"
+pavithra.education = "B.A HISTORY"
+pavithra.family_members =
+
+
+print(pavithra.__dict__)
+
+bharath = Intro()
+bharath.name ="'bharath"
+bharath.native ="bengalore"
+bharath.education = "eight"
+bharath.family_members = 4
+
+print(bharath.family_members)
+
+import unittest
+
+
