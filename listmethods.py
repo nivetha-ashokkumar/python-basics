55771cfe0f771c6fd93d9a4a5c05b510894c1112
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 19:47:44 +0530

    list methods commit

---
+++ listmethods.py
@@ -0,0 +1,55 @@
+stulist =["ram", "anitha", "murali"]
+stulist.append("ashokkumar")
+print(stulist)
+
+
+stulist = ["ram", "anitha", "murali", "anitha"]
+stulist.count("anitha")
+print(stulist)
+
+ 
+stulist = ["ram", "anitha"]
+stuage =[21, 20]
+stulist.extend(stuage)
+print(stulist)
+
+stulist = ["ram", "anitha"]
+stulist.index("ram")
+print(stulist)
+
+stulist = ["ram", "anitha"]
+stulist.insert(2, "ashokkumar")
+print(stulist)
+
+stulist = ["ram", "anitha"]
+stulist.pop()
+print(stulist)
+
+stulist = ["ram", "anitha", "ashokkumar"]
+stulist.pop(1)
+print(stulist)
+
+stulist = ["ram", "anitha", "ashokkumar"]
+stulist.remove("anitha")
+print(stulist)
+
+stulist = ["ram", "anitha", "ashokkumar"]
+stulist.reverse()
+print(stulist)
+
+stulist = [2, 5, 6, 7, 4,1]
+stulist.sort()
+print(stulist)
+
+
+
+
+
+
+
+
+
+
+
+
+    
