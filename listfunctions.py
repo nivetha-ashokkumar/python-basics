76cc7b4a1700019cde6d796956cf681ee1ad4815
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 19:40:10 +0530

    list functions commit

---
+++ listfunctions.py
@@ -0,0 +1,18 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+print("the length of the list is:", len(my_list))
+print("the maximum of the list is:", max(my_list))
+print("the minimum of the list is:", min(my_list))
+
+my_list1 = ("anitha", 21, 2000)
+print(list(my_list1))
+
+list1 = [1,2,3,4]
+list2 = [1,2,3,4]
+print("the comparision of the list1 and list2 is:", cmp(list1, list2))
+
+
+
+
+
+
+    
