80b40e2d0b7fb77337b00f358b85c86e60b9fc20
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 21:40:58 +0530

    set methods commit

---
+++ setmethods (1).py
@@ -0,0 +1,16 @@
+set = {1, 2, 3, 4, 5}
+print(set)
+
+set = {"nivetha", "anitha", "karthick"}
+print(len(set))
+
+set = {3, 4, 1, 5}
+print(min(set))
+
+set = {3, 4, 1, 5}
+print(max(set))
+
+set = {3, 4, 1, 5}
+print(sorted(set))
+
+
