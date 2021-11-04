c0bcdac4e8895b64b29439024a41bc5863f5dc00
nivetha-ashokkumar <nivetha7620@gmail.com>
2021-11-04 21:42:18 +0530

    tuple function commit

---
+++ tuplefunction.py
@@ -0,0 +1,17 @@
+t1 = (1, "hello", "hai")
+print(len(t1))
+
+t1 = (1, 2, 3, 4, 5 )
+print(max(t1))
+
+t1 = (1, 2, 3, 4, 5 )
+print(min(t1))
+
+t1 = (1, 8, 3, 7, 5 )
+print(sorted(t1))
+
+t1 = (1, 8, 3, 7, 5 )
+print(sum(t1))
+
+t1 = (1, 8, 3, 7, 5 )
+print(tuple())
