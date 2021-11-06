08dc13b6b225b2ea5a5c52d35ebea3ccd639f438
nivetha-ashokkumar <93514418+nivetha-ashokkumar@users.noreply.github.com>
2021-11-06 18:51:39 +0530

    Update addition_operation.py
    this describe the addition of two numbers.

--- addition_operation.py
+++ addition_operation.py
@@ -1,9 +1,13 @@
-def sum(num1, num2):
-    sum = num1 + num2
+def sum(number1, number2):
+    sum = number1 + number2
     return sum
+
+
 num1 = int(input("enter first number:"))
 num2 = int(input("enter second number:"))
-result = sum(num1, num2)
+
+
+result = sum(number1, number2)
 print(result)
 
 
