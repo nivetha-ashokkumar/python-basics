ee191d0dc5b4eab9f1ccf91ae49f74224c72fd7d
nivetha-ashokkumar <93514418+nivetha-ashokkumar@users.noreply.github.com>
2021-11-06 18:48:31 +0530

    Update and rename addition_of_integer_float.py to sum_of_int_float.py
    This code describe addition of integer  and float.  Then print the type of sum.

---
+++ sum_of_int_float.py
@@ -0,0 +1,7 @@
+number1 = int (input("enter first number"))
+number2 = float(input("enter second number"))
+
+sum = int(number1) + float(number2)
+
+print("the addition of two numbers are:", sum)
+print(type(sum))
