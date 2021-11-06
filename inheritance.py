9799a64fc123bf9f69c01ffc543635a2e801700e
nivetha-ashokkumar <93514418+nivetha-ashokkumar@users.noreply.github.com>
2021-11-06 20:12:18 +0530

    Update inheritance.py
    this describe the inheritance concepts.

--- inheritance.py
+++ inheritance.py
@@ -2,10 +2,13 @@ class shape:
     def __init__(self, height, width):
         self.height = height
         self.width = width
+        
     def area(self):
         print(self.height, self.width)
+        
 class rectangle(shape):
     def __init__(self, height, width):
         super().__init__(height,width)
+        
 x = rectangle(7, 8)
 x.area()
