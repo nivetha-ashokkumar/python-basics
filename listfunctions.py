f16fd35cbbaded92f63715d6fe86a39eb0f83c3c
nivetha-ashokkumar <93514418+nivetha-ashokkumar@users.noreply.github.com>
2021-11-06 20:20:25 +0530

    Update listfunctions.py
    this describe the functions based on list.

--- listfunctions.py
+++ listfunctions.py
@@ -1,9 +1,11 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+
 print("the length of the list is:", len(my_list))
 print("the maximum of the list is:", max(my_list))
 print("the minimum of the list is:", min(my_list))
 
 my_list1 = ("anitha", 21, 2000)
+
 print(list(my_list1))
 
 
