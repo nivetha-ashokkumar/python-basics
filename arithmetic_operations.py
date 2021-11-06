4bb12f0c13bc17460f1550520381feccddf15504
nivetha-ashokkumar <93514418+nivetha-ashokkumar@users.noreply.github.com>
2021-11-06 19:06:23 +0530

    Update and rename arithmetic_operation.py to arithmetic_operations.py
    this describe the arithmetic operations like addition, subtraction, multiplication, division and modulus.

---
+++ arithmetic_operations.py
@@ -0,0 +1,22 @@
+number1 = int(input("enter your first number is:"))
+number2 = int(input("enter your second number is:"))
+
+sum = number1 + number2
+
+print("the addition o two numbers are", sum)
+
+sub = number1 - number2
+
+print("the subtraction of two numbers are", sub)
+
+mul = number1 * number2
+
+print("the multiplication of two numbers are", mul)
+
+div = number1 / number2
+
+print("the division of two numbers are", div)
+
+mod = number1 % number2
+
+print("the modulus of two numbers")
