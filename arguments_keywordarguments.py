d11444bd491d3c6b718b160b677b8da28b7c1ff1
nivetha-ashokkumar <93514418+nivetha-ashokkumar@users.noreply.github.com>
2021-11-06 19:00:35 +0530

    Update arguments_keywordarguments.py

--- arguments_keywordarguments.py
+++ arguments_keywordarguments.py
@@ -2,8 +2,8 @@ def sum(*arguments, **keywordarguments):
     sum = arguments1 + keywordarguments1
     return sum
 
-arguments1 = int(input("enter args value:"))
-keywordarguments1 = int(input("enter kwargs value:"))
+arguments1 = int(input("enter arguments value:"))
+keywordarguments1 = int(input("enter keywordarguments value:"))
 
 result = sum(arguments1, keywordarguments1)
 print(result)
