e61a3037e2f542208d1ba370e541f717c9399e53
nivetha-ashokkumar <93514418+nivetha-ashokkumar@users.noreply.github.com>
2021-11-08 21:57:35 +0530

    this is my first commit

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+number = int(input("Enter positive integer:"))
+number1 = 0
+number2 = 1
+number3 = 2
+print(number1)
+print(number2)
+while number3 < number:
+    number4 = number1 + number2
+    print(number4)
+    number1 = number2
+    number2 = number4
+    number3 = number3 + 1
